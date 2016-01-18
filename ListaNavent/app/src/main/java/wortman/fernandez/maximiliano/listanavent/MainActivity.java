6e07c5ebbdbc53c3b60403579ff568456658f836
Maximiliano <maxifwortman@gmail.com>
2016-01-18 16:04:37 -0300

    primer commit

---
+++ ListaNavent/app/src/main/java/wortman/fernandez/maximiliano/listanavent/MainActivity.java
@@ -0,0 +1,59 @@
+package wortman.fernandez.maximiliano.listanavent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ListView list;
+    String[] itemname ={
+            "San fernando",
+            "San Isidro",
+            "Martinez",
+            "Escobar",
+            "Pilar",
+            "Vicente Lopez",
+            "Tigre",
+            "Palomar"
+    };
+
+    Integer[] imgid={
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6,
+            R.drawable.pic7,
+            R.drawable.pic8,
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
